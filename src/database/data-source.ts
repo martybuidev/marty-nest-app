a3cf3f702d5cb808d9ca1bc9ebcb94748662e90c
marty <marty.bui.goldenowl@gmail.com>
2026-07-31 14:11:08 +0700

    feat: update useFactory return value by re-use dbOptions

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -5,11 +5,14 @@ import { DataSource } from 'typeorm';
 import { databaseConfig } from '@/config/config';
 
 dotenv.config();
-const dbOptions = databaseConfig();
+
+export const dbOptions = {
+  ...databaseConfig(),
+  entities: [path.join(__dirname, '..', 'modules', '**', '*.entity.{js,ts}')],
+  synchronize: false,
+};
 
 export const AppDataSource = new DataSource({
   ...dbOptions,
   migrations: [path.join(__dirname, 'migrations', '*.{js,ts}')],
-  entities: [path.join(__dirname, '..', 'modules', '**', '*.entity.{js,ts}')],
-  synchronize: false,
 });
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,29 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import path from 'path';
-
-import { ConfigService } from '@/config/config.service';
+import { dbOptions } from './data-source';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (appConfigService: ConfigService) => {
-        const { database: dbConfig } = appConfigService;
-        return {
-          type: dbConfig.type,
-          host: dbConfig.host,
-          port: dbConfig.port,
-          username: dbConfig.username,
-          password: dbConfig.password,
-          database: dbConfig.database,
-          entities: [
-            path.join(__dirname, '..', 'modules', '**', '*.entity.{js,ts}'),
-          ],
-          synchronize: false,
-        };
-      },
+      useFactory: () => dbOptions,
     }),
   ],
 })
