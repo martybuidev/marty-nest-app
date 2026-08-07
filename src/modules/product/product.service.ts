a161595ca17d8d5cf7f3957e95694ebd72898c4a
marty <marty.bui.goldenowl@gmail.com>
2026-08-07 09:35:24 +0700

    refact: change image module into storage module

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -41,13 +41,13 @@ export class ProductService {
   }
 
   async findAll() {
-    return await this.productRepository.find({ relations: { images: true } });
+    return await this.productRepository.find({ relations: { storages: true } });
   }
 
   async findOne(id: number) {
     return await this.productRepository.findOneOrFail({
       where: { id },
-      relations: { images: true },
+      relations: { storages: true },
     });
   }
 
