90809813b91158e9cc843cb7397f6d6a1aa51224
marty <marty.bui.goldenowl@gmail.com>
2026-08-10 11:11:24 +0700

    fix: resolve confict

--- src/modules/storage/dto/response-storage.dto.ts
+++ src/modules/storage/dto/response-storage.dto.ts
@@ -1,12 +1,9 @@
 import { Expose } from 'class-transformer';
 
 export class ResponseStorageDto {
-<<<<<<< HEAD
   @Expose()
   id: number;
 
-=======
->>>>>>> 21e5938 (refact: change image module into storage module)
   @Expose()
   name: string;
 
